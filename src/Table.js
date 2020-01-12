51c4800e7d99d7df6487cda3a176c4da9120fecd
pratikpatelx <pratikpatel2369@outlook.com>
2020-01-11 23:12:42 -0600

    first commit

---
+++ src/Table.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Component } from "react";
+
+/* import React, {Component} from 'react'
+
+class Table extends Component{
+    render(){
+        return(
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <td>Job</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Charlie</td>
+                        <td>Janitor</td>
+                    </tr>
+                    <tr>
+                        <td>Mac</td>
+                        <td>Bouncer</td>
+                    </tr>
+                    <tr>
+                        <td>Dee</td>
+                        <td>Aspiring Actress</td>
+                    </tr>
+                    <tr>
+                        <td>Dennis</td>
+                        <td>Bartender</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+
+    }
+}
+
+export default Table */
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            <td>
+                <button onClick={() => props.removeCharacter(index)}>Delete</button>
+            </td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
+}
+
+class Table extends Component{
+    render() {
+        const {characterData, removeCharacter} = this.props
+
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+            </table>
+        )
+    }
+}
+
+export default Table;
